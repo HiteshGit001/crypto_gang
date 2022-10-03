c6fb9668554d718d016502bc0b19d7ac69c13608
hiteshgn009 <hiteshgn009@gmail.com>
2022-10-03 14:23:45 +0530

    added route when i click on trending coin

--- src/dataContext/Context.js
+++ src/dataContext/Context.js
@@ -7,10 +7,12 @@ const Context = ({ children }) => {
   const [currency,setCurrency] = useState("inr");
   const [symbol, setSymbol] = useState("₹");
   const [trending, setTrending] = useState([]);
+  const [selectedCoins, setSelectedCoins] = useState('');
   const values ={
     trending, setTrending,
     currency, setCurrency,
     symbol, setSymbol,
+    selectedCoins, setSelectedCoins,
   }
   return (
     <ContextData.Provider value={values}>
--- src/services/fetchapi.service.js
+++ src/services/fetchapi.service.js
@@ -1,8 +1,15 @@
-import { TrendingCoin } from '../config/api'
+import { SelectedCoin, TrendingCoin } from '../config/api'
 
 export const fetchTrending = async(currency) => {
   const response = await fetch(TrendingCoin(currency))
   const json = await response.json();
-  console.log(json,'json');
+  // console.log(json,'json');
+  return json;
+}
+
+export const fetchCoins = async(coinID) => {
+  const response = await fetch(SelectedCoin(coinID))
+  const json = await response.json();
+  console.log(json, "jsfghon");
   return json;
 }
