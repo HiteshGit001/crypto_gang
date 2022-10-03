e76dc0b90fc17b9c53fa971759508a9f6ed28b6e
hiteshgn009 <hiteshgn009@gmail.com>
2022-10-03 11:05:01 +0530

    added symbol and profit

--- src/components/CoinCard/CoinCard.jsx
+++ src/components/CoinCard/CoinCard.jsx
@@ -3,11 +3,13 @@ import { Card, makeStyles } from '@mui/material';
 import styles from "./CoinCard.module.css";
 import { useData } from '../../dataContext/Context';
 
-const CoinCard = ({ icon, price }) => {
-  const{symbol} = useData();
+const CoinCard = ({ icon, price, symbols, profit, priceChangePercent }) => {
+  const { symbol } = useData();
   return (
     <Card className={styles.coin_card}>
       <img className={styles.cr_img} src={icon} alt="" />
+      <p className={styles.symbols}>{symbols}</p>
+      <p className={profit?styles.profit:styles.loss}>{priceChangePercent}%</p>
       <p>{symbol}{price}</p>
     </Card>
   )
